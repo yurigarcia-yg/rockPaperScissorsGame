a5aa2bb3d8556278481b0da0f4a5f6de6422a7dc
Yuri Garcia <yurigarcia.yg@outlook.com>
2024-06-26 16:48:45 -0400

    Add UI for Rock Paper Scissors game

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,7 +2,26 @@
 
 let humanScore = 0;
 let computerScore = 0;
-let round = 1;
+let round = 2;
+
+//music and sounds
+let rockSound = new Audio("./music/rock.mp3");
+let paperSound = new Audio("./music/paper.mp3");
+let scissorsSound = new Audio("./music/scissors.mp3");
+let clapping = new Audio("./music/clapping.mp3");
+document.addEventListener("click", () => {
+  let backgroundMusic = document.querySelector(".background-music");
+  backgroundMusic.volume = 0.2;
+  backgroundMusic.play();
+});
+//DOM manipulation
+let results = document.querySelector(".results");
+let rock = document.querySelector(".rock-img");
+let paper = document.querySelector(".paper-img");
+let scissors = document.querySelector(".scissors-img");
+let buttonsArray = [rock, paper, scissors];
+
+let newGame = document.querySelector(".new-game");
 
 //need to get computer input
 function getComputerChoice() {
@@ -19,90 +38,167 @@ function getComputerChoice() {
 
   return choice;
 }
-//need to get human input
-function getHumanChoice() {
-  let choice = prompt(
-    `Choose Rock, Paper, or Scissors - Round ${round} - Score: ${humanScore} - AI Score: ${computerScore}`,
-    ""
-  );
-  if (choice === null) {
-    return choice;
-  }
-  return choice.toLowerCase().trim();
-}
+
 //logic for the game
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
+  startGame();
 
-    if (humanChoice === null) {
-      break;
-    } else if (
-      humanChoice !== "rock" &&
-      humanChoice !== "paper" &&
-      humanChoice !== "scissors"
-    ) {
-      alert("Not a valid choice, Game exiting. Refresh page to restart.");
-      break;
-    } else {
-      playRound(humanChoice, computerChoice);
-      round++;
-    }
-  }
+  //starts new game
+  newGame.addEventListener("click", () => {
+    results.innerHTML = `<span class="divider">|</span>
+          Choose Your Fighter! <span class="divider">|</span> Best of 5 Rounds
+          
+          Wins <span class="divider">|</span> Round: 1
+          <span class="divider">|</span>`;
+    rock.style.pointerEvents = "auto";
+    paper.style.pointerEvents = "auto";
+    scissors.style.pointerEvents = "auto";
+    rock.src = "./rps-colors/rock-black.svg";
+    paper.src = "./rps-colors/paper-black.svg";
+    scissors.src = "./rps-colors/scissors-black.svg";
+    humanScore = 0;
+    computerScore = 0;
+    round = 2;
+    newGame.style.backgroundColor = "blue";
+    setTimeout(() => {
+      newGame.style.backgroundColor = "white";
+    }, 500);
+  });
 
-  if (round === 6 && humanScore > computerScore) {
-    alert(`Victory - Score: ${humanScore} - AI Score: ${computerScore}`);
-  } else if (round === 6 && humanScore === computerScore) {
-    alert(`Stalemate - Score: ${humanScore} - AI Score: ${computerScore}`);
-  } else if (round === 6 && humanScore === computerScore) {
-    alert(`Defeat - Score: ${humanScore} - AI Score: ${computerScore}`);
-  }
+  //round functionality
+
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock") {
       if (computerChoice === "paper") {
-        alert("Round Loss! Paper beats rock");
+        results.innerHTML = `<span class="divider">|</span> Round Loss! <span class="divider">|</span> Paper beats rock <span class="divider">|</span>`;
+        rock.src = "./rps-colors/rock-red.svg";
         computerScore++;
       } else if (computerChoice === "rock") {
-        alert("Round Draw! Rock can't beat rock");
-        computerScore += 0.5;
-        humanScore += 0.5;
+        results.innerHTML = `<span class="divider">|</span>Round Draw! <span class="divider">|</span> Rock can't beat rock <span class="divider">|</span>`;
+        rock.src = "./rps-colors/rock-gray.svg";
       } else if (computerChoice === "scissors") {
-        alert("Round Win! Rock beats scissors");
+        results.innerHTML = `<span class="divider">|</span> Round Win! <span class="divider">|</span> Rock beats scissors <span class="divider">|</span>`;
+        rock.src = "./rps-colors/rock-green.svg";
         humanScore++;
       }
     } else if (humanChoice === "paper") {
       if (computerChoice === "paper") {
-        alert("Round Draw! Paper can't beat paper");
-        computerScore += 0.5;
-        humanScore += 0.5;
+        results.innerHTML = `<span class="divider">|</span> Round Draw! <span class="divider">|</span> Paper can't beat paper <span class="divider">|</span>`;
+        paper.src = "./rps-colors/paper-gray.svg";
       } else if (computerChoice === "rock") {
-        alert("Round Win! Paper beats rock");
-
+        results.innerHTML = `<span class="divider">|</span> Round Win! <span class="divider">|</span> Paper beats rock <span class="divider">|</span>`;
+        paper.src = "./rps-colors/paper-green.svg";
         humanScore++;
       } else if (computerChoice === "scissors") {
-        alert("Round Loss! Paper can't beat scissors");
+        results.innerHTML = `<span class="divider">|</span>Round Loss! <span class="divider">|</span> Paper can't beat scissors <span class="divider">|</span>`;
+        paper.src = "./rps-colors/paper-red.svg";
         computerScore++;
       }
     } else if (humanChoice === "scissors") {
       if (computerChoice === "paper") {
-        alert("Round Win! Scissors beats paper");
+        results.innerHTML = `<span class="divider">|</span> Round Win! <span class="divider">|</span> Scissors beats paper <span class="divider">|</span>`;
+        scissors.src = "./rps-colors/scissors-green.svg";
         humanScore++;
       } else if (computerChoice === "rock") {
-        alert("Round Loss! Scissors can't beat");
+        results.innerHTML = `<span class="divider">|</span>Round Loss! <span class="divider">|</span> Scissors can't beat <span class="divider">|</span>`;
+        scissors.src = "./rps-colors/scissors-red.svg";
         computerScore++;
       } else if (computerChoice === "scissors") {
-        alert("Round Draw! Scissors can't beat scissors");
-        computerScore += 0.5;
-        humanScore += 0.5;
+        results.innerHTML = `<span class="divider">|</span> Round Draw! <span class="divider">|</span> Scissors can't beat scissors <span class="divider">|</span>`;
+        scissors.src = "./rps-colors/scissors-gray.svg";
       }
     }
   }
 
-  humanScore = 0;
-  computerScore = 0;
-  round = 1;
+  function startGame() {
+    for (i = 0; i < buttonsArray.length; i++) {
+      buttonsArray[i].addEventListener("click", (e) => {
+        let target = e.target.closest("img");
+        let choice = target.id;
+        let option = "";
+        if (choice === "rock-img") {
+          option = "rock";
+          choice = rock;
+          rock.style.pointerEvents = "none";
+          paper.style.pointerEvents = "none";
+          scissors.style.pointerEvents = "none";
+          rockSound.play();
+          newGame.disabled = true;
+          newGame.style.color = "black";
+        } else if (choice === "paper-img") {
+          option = "paper";
+          choice = paper;
+          rock.style.pointerEvents = "none";
+          paper.style.pointerEvents = "none";
+          scissors.style.pointerEvents = "none";
+          paperSound.play();
+          newGame.disabled = true;
+          newGame.style.color = "black";
+        } else if (choice === "scissors-img") {
+          option = "scissors";
+          choice = scissors;
+          rock.style.pointerEvents = "none";
+          paper.style.pointerEvents = "none";
+          scissors.style.pointerEvents = "none";
+          scissorsSound.play();
+          newGame.disabled = true;
+          newGame.style.color = "black";
+        }
+
+        playRound(option, getComputerChoice());
+
+        if (round > 5) {
+          if (humanScore > computerScore) {
+            results.innerHTML = `<span class="divider">|</span> Victory! <span class="divider">|</span> Player Score: ${humanScore} <span class="divider">|</span>
+    AI Score: ${computerScore} <span class="divider">|</span>`;
+            rock.style.pointerEvents = "none";
+            paper.style.pointerEvents = "none";
+            scissors.style.pointerEvents = "none";
+            rock.src = "./rps-colors/rock-green.svg";
+            paper.src = "./rps-colors/paper-green.svg";
+            scissors.src = "./rps-colors/scissors-green.svg";
+            newGame.disabled = false;
+            clapping.play();
+          } else if (humanScore < computerScore) {
+            results.innerHTML = `<span class="divider">|</span> Defeat! <span class="divider">|</span> Player Score: ${humanScore} <span class="divider">|</span>
+    AI Score: ${computerScore} <span class="divider">|</span>`;
+            rock.style.pointerEvents = "none";
+            paper.style.pointerEvents = "none";
+            scissors.style.pointerEvents = "none";
+            rock.src = "./rps-colors/rock-red.svg";
+            paper.src = "./rps-colors/paper-red.svg";
+            scissors.src = "./rps-colors/scissors-red.svg";
+            newGame.disabled = false;
+          } else if (humanScore === computerScore) {
+            results.innerHTML = `<span class="divider">|</span> Stalemate <span class="divider">|</span> Player Score: ${humanScore} <span class="divider">|</span>
+    AI Score: ${computerScore} <span class="divider">|</span>`;
+            rock.style.pointerEvents = "none";
+            paper.style.pointerEvents = "none";
+            scissors.style.pointerEvents = "none";
+            rock.src = "./rps-colors/rock-gray.svg";
+            paper.src = "./rps-colors/paper-gray.svg";
+            scissors.src = "./rps-colors/scissors-gray.svg";
+            newGame.disabled = false;
+          }
+        } else {
+          choice.style.pointerEvents = "none";
+          setTimeout(() => {
+            results.innerHTML = `<span class="divider">|</span> Round: ${round} <span class="divider">|</span> Player Score: ${humanScore} <span class="divider">|</span> AI Score: ${computerScore} <span class="divider">|</span>`;
+            round++;
+
+            if (choice === rock || choice === paper || choice === scissors) {
+              choice.src = `./rps-colors/${option}-black.svg`;
+              rock.style.pointerEvents = "auto";
+              paper.style.pointerEvents = "auto";
+              scissors.style.pointerEvents = "auto";
+              newGame.disabled = false;
+            }
+          }, 1500);
+        }
+      });
+    }
+  }
 }
 
 playGame();
